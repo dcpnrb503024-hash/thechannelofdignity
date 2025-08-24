5cf3e32e76a573dffe9a677f1d6170288dd0804c
dcpnrb503024-hash <dcpnrb503024@spu.ac.ke>
2025-08-24 22:52:23 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ChannelOfDignity from "./ChannelOfDignity";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChannelOfDignity />
+  </React.StrictMode>
+);
